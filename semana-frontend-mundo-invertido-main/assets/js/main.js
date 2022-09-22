06a87e9dfbee7ca4f6563559de465502621a8548
marceloneumann81 <marneumann@gmail.com>
2022-09-22 18:32:16 -0300

    validacao de campos do formulario

--- semana-frontend-mundo-invertido-main/assets/js/main.js
+++ semana-frontend-mundo-invertido-main/assets/js/main.js
@@ -7,14 +7,58 @@ const txtEmail = document.getElementById('txtEmail');
 const txtLevel = document.getElementById('txtLevel');
 const txtCharacter = document.getElementById('txtCharacter');
 const btnSubscribe = document.getElementById('btnSubscribe');
-
 const subscriptionsList = document.getElementById('subscriptions');
+
+
+const labelName = document.getElementById('labelName');
+const labelEmail = document.getElementById('labelEmail');
+const labelLevel = document.getElementById('labelWhats');
+const labelCharacter = document.getElementById('labelCharacter');
+
+
+const messageError = document.getElementById('messageError');
+const messageSuccess = document.getElementById('messageSuccess');
+
+
+
+txtName.addEventListener('keyup', function() {
+    if ( txtName.value.length <= 2 && txtName.value.length > 0) {
+        labelName.style.color = 'red';
+    }
+    else {
+        labelName.style.color = '';
+    }
+    
+    
+    
+})
+
+
+// VALIDACAO CAMPO E-MAIL
+txtEmail.addEventListener('blur', function(){
+    const exp = /\S+@\S+\.\S+/
+    if ( txtEmail.value.length > 0 && !exp.test(txtEmail.value)) {
+        alert('Endereço de e-mail inválido')
+        txtEmail.value = '';
+    }    
+})
+
+
+txtWhats.addEventListener('blur', function() {
+    const exp = /\(\d{2}\)\ \d{5}\-\d{4}/
+    console.log(txtWhats.value)
+    if(!exp.test(txtWhats.value)) {
+        alert('Numero do whats digitado é inválido')
+        txtWhats.value = ''        
+    }
+
+})
 
 btnSubscribe.addEventListener('click', async () => {
     const subscription = {
         name: txtName.value,
         email: txtEmail.value,
-        level: txtLevel.value,
+        whats: txtWhats.value,
         character: txtCharacter.value
     }
 
@@ -23,7 +67,7 @@ btnSubscribe.addEventListener('click', async () => {
 
     txtName.value = '';
     txtEmail.value = '';
-    txtLevel.value = '';
+    txtWhats.value = '';
     txtCharacter.value = '';
 
     alert('Inscrito com sucesso no nosso clube!!!')
@@ -43,4 +87,5 @@ async function loadData(){
 
 console.log(subscriptions)
 }
+
 
